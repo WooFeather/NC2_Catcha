f6f0680b59eb8fa07238495b8dc616cec819c1b9
woboneyo <bluewing1004@gmail.com>
2024-06-14 11:21:15 +0900

    Initial Commit

---
+++ Catcha/CatchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchaApp.swift
+//  Catcha
+//
+//  Created by 조우현 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
