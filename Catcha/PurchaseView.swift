f700ccc8fbe79d9ee84637180e39dec4aaf430bf
woboneyo <bluewing1004@gmail.com>
2024-06-14 11:53:25 +0900

    test

---
+++ Catcha/PurchaseView.swift
@@ -0,0 +1,18 @@
+//
+//  PurchaseView.swift
+//  Catcha
+//
+//  Created by 조우현 on 6/14/24.
+//
+
+import SwiftUI
+
+struct PurchaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PurchaseView()
+}
